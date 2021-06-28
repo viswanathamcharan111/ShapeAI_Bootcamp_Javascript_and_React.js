8d3ff0f74866b2c77f0e7d3835b873c64fe35336
viswanathamcharan111 <73576992+viswanathamcharan111@users.noreply.github.com>
2021-06-28 11:51:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Web Dev Javascript and React.js</h1>
+      <p>A basic 7-days web dev React.js Bootcamp by Shaurya Sinha</p>
+    </div>
+  );
+}
+
+export default Note;
